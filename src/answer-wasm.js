eaa51fccdd82a3d9240f3ab2c66264d2b020fbbb
devcronberg <devcronberg@gmail.com>
2019-11-06 21:49:39 +0100

    Update answer-wasm.js

--- src/answer-wasm.js
+++ src/answer-wasm.js
@@ -1,14 +1,15 @@
 let answerWASM;
+// Hent funktion fra WASM
 fetch("main.wasm")
   .then(response => response.arrayBuffer())
   .then(bytes => WebAssembly.instantiate(bytes))
   .then(results => {
-    console.log("*");
     instance = results.instance;
     answerWASM = instance.exports.answerWASM;
   })
   .catch(console.error);
 
+// Når der klikkes på knappen!!
 document.getElementById("btnJsWasm").onclick = function() {
   document.getElementById("result").innerText = "Answer from WASM/JS: " + answerWASM();
 };
