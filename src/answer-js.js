5883797f2f5fe01936fc48079da3f577d1858404
devcronberg <devcronberg@gmail.com>
2019-11-06 21:50:18 +0100

    Update answer-js.js

--- src/answer-js.js
+++ src/answer-js.js
@@ -1,7 +1,9 @@
+// Svaret på alt
 function answerJS() {
   return 42;
 }
 
+// Når der klikkes på knappen!!
 document.getElementById("btnJs").onclick = function() {
   document.getElementById("result").innerText = "Answer from JS: " + answerJS();
 };
